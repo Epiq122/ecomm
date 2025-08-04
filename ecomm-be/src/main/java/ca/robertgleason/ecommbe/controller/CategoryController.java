475446e3aad102091d5b1925f779cdad335cc557
Robert Gleason <l2ob122@gmail.com>
2025-08-04 14:14:41 -0700

    add Category model and CategoryController for category management

---
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/controller/CategoryController.java
@@ -0,0 +1,21 @@
+package ca.robertgleason.ecommbe.controller;
+
+
+import ca.robertgleason.ecommbe.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private final List<Category> categories = new ArrayList<>();
+
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+}
