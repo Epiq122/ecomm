cb53de9541056c2e0446bfefd0048ee5efcb6ca8
Robert Gleason <l2ob122@gmail.com>
2025-08-04 13:55:36 -0700

    init

---
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/EcommBeApplication.java
@@ -0,0 +1,13 @@
+package ca.robertgleason.ecommbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommBeApplication.class, args);
+    }
+
+}
---
+++ ecomm-be/src/test/java/ca/robertgleason/ecommbe/EcommBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.robertgleason.ecommbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
