14baec571ea45553754d251a761161126f2fc768
Robert Gleason <l2ob122@gmail.com>
2025-08-04 14:36:33 -0700

    refactor CategoryController to use CategoryService for category management; add CategoryService and CategoryServiceImpl for business logic

---
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/service/CategoryService.java
@@ -0,0 +1,14 @@
+package ca.robertgleason.ecommbe.service;
+
+
+import ca.robertgleason.ecommbe.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CategoryService {
+    List<Category> getAllCategories();
+
+    void createCategory(Category category);
+}
---
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/service/CategoryServiceImpl.java
@@ -0,0 +1,26 @@
+package ca.robertgleason.ecommbe.service;
+
+
+import ca.robertgleason.ecommbe.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final List<Category> categories = new ArrayList<>();
+
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categories.add(category);
+
+    }
+}
