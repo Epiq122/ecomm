cca2d3e0cfd682547aa176d78f7d80757f5944c6
Robert Gleason <l2ob122@gmail.com>
2025-08-07 19:35:06 -0700

    feat: add ProductDTO and ProductResponse classes for product data handling

---
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/payload/ProductDTO.java
@@ -0,0 +1,19 @@
+package ca.robertgleason.ecommbe.payload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private Double price;
+    private Double discount;
+    private Double specialPrice;
+}
