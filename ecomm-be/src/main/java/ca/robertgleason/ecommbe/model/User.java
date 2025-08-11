2a3c60fb42ca125269ba464239d8fb69301da7a0
Robert Gleason <l2ob122@gmail.com>
2025-08-10 21:52:50 -0700

    added relationship between role and users

--- ecomm-be/src/main/java/ca/robertgleason/ecommbe/model/User.java
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/model/User.java
@@ -6,7 +6,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -30,9 +35,19 @@ public class User {
     @Size(max = 120)
     private String password;
 
+
+    @Getter
+    @Setter
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
+
 }
