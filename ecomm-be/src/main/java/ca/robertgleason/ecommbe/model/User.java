dc4ff756a8f7e32908563b3c2236b83a2b43ffd8
Robert Gleason <l2ob122@gmail.com>
2025-08-11 18:16:48 -0700

    feat: add unique constraints for username and email in User entity

--- ecomm-be/src/main/java/ca/robertgleason/ecommbe/model/User.java
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/model/User.java
@@ -16,7 +16,10 @@ import java.util.Set;
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,6 +45,7 @@ public class User {
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
+
     private Set<Role> roles = new HashSet<>();
 
     public User(String username, String email, String password) {
