4af3cbde9b5d29f46fbb3adec68428fe7006f5f9
Robert Gleason <l2ob122@gmail.com>
2025-08-12 22:18:02 -0700

    feat: add Cart and CartItem entities to model the shopping cart system

---
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/model/Cart.java
@@ -0,0 +1,35 @@
+package ca.robertgleason.ecommbe.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "cart")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    private Double totalPrice = 0.0;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    @OneToMany(mappedBy = "cart", cascade = {CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.MERGE}, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+
+}
