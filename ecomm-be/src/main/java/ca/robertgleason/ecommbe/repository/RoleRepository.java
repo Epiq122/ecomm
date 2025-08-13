5665d42d51cc4755e9a503fa5ade65286ab6f528
Robert Gleason <l2ob122@gmail.com>
2025-08-12 19:13:15 -0700

    feat: add findByRoleName method to RoleRepository for role retrieval by AppRole

--- ecomm-be/src/main/java/ca/robertgleason/ecommbe/repository/RoleRepository.java
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/repository/RoleRepository.java
@@ -1,9 +1,13 @@
 package ca.robertgleason.ecommbe.repository;
 
+import ca.robertgleason.ecommbe.model.AppRole;
 import ca.robertgleason.ecommbe.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 
+    Optional<Role> findByRoleName(AppRole appRole);
 }
