06946c0d8fa8b4e0b941112a03d8c9cbc026f0e7
Robert Gleason <l2ob122@gmail.com>
2025-08-15 21:14:46 -0700

    feat: establish relationships for Cart and CartItem in Product and User models
    - Added a one-to-one relationship between User and Cart.
    - Added a one-to-many relationship between Product and CartItem for improved data modeling.

--- ecomm-be/src/main/java/ca/robertgleason/ecommbe/model/User.java
+++ ecomm-be/src/main/java/ca/robertgleason/ecommbe/model/User.java
@@ -60,6 +60,12 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "address_id"))
     private List<Address> addresses = new ArrayList<>();
 
+
+    @ToString.Exclude
+    @OneToOne(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private Cart cart;
+
+
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
